15de3abe5c67c45badc754b69db91c0b6d595fa9
VSocks <marvinpsilva2003@gmail.com>
2024-03-26 17:54:30 -0300

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz{
+    public static void main(String[] args) {
+        for(int i = 0; i <= 100; i++){
+            String result = "";
+            if(i % 3 == 0)
+                result += "Fizz";
+            if(i % 5 == 0)
+                result += "Buzz";
+            if(result == "")
+                result = String.valueOf(i);
+            System.out.println(result);
+        }
+    }
+}
